7c0bddf2309a0ebbe39d14eb6d2396d66869995e
ElangoManikandan <108584490+ElangoManikandan@users.noreply.github.com>
2025-04-17 08:32:44 +0530

    Update HTML5toTouch.js

--- src/dnd/HTML5toTouch.js
+++ src/dnd/HTML5toTouch.js
@@ -4,9 +4,11 @@ import { HTML5Backend } from 'react-dnd-html5-backend';
 export const HTML5toTouch = {
   backends: [
     {
+      id: 'html5', // ✅ added ID
       backend: HTML5Backend,
     },
     {
+      id: 'touch', // ✅ added ID
       backend: TouchBackend,
       options: { enableMouseEvents: true },
       preview: true,
